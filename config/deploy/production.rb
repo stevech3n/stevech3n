7c403cf8fdebb9dd97725178d31b1a4b03668355
stevech3n <stevech3n@gmail.com>
2014-05-02 23:42:55 -0500

    third test

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,8 +2,8 @@
 role :app, %w{stevechen@stevech3n.com}
 role :web, %w{stevechen@stevech3n.com}
 
-server 'stevechen.com', user: 'stevechen', roles: %w{web app}
+server 'stevech3n.com', user: 'stevechen', roles: %w{web app}
 
 set :user, 'stevechen'
 
-set :deploy_to, '/home/root/sites/stevechen'
+set :deploy_to, '/home/root/sites/stevech3n'
