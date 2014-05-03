c09d059a8ce629c40fffb096bfbf06bed524a5eb
stevech3n <stevech3n@gmail.com>
2014-05-02 23:11:16 -0500

    second test

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
 
-role :app, %w{root@stevech3n.com}
-role :web, %w{root@stevech3n.com}
+role :app, %w{stevechen@stevech3n.com}
+role :web, %w{stevechen@stevech3n.com}
 
-server 'stevechen.com', user: 'root', roles: %w{web app}
+server 'stevechen.com', user: 'stevechen', roles: %w{web app}
 
-set :user, 'root'
+set :user, 'stevechen'
 
 set :deploy_to, '/home/root/sites/stevechen'
